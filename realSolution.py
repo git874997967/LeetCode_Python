68ba0e434d58b629da212e21eb5fd421e08f9a76
unknown <g8749979677@gmail.com>
2021-03-19 00:09:22 -0700

    optmize

--- realSolution.py
+++ realSolution.py
@@ -2,20 +2,21 @@ def get_free_time(cal1,cal2,duration,time):
     free1,free2 =  [], []
     def get_free(cal,time):
         busy,freeTime = list(cal[0]),[]
+        if cal[0][0] != time[0]:
+            freeTime.append([time[0],cal[0][0]])
         for i in range(1,len(cal)):
             if cal[i][0] > busy[1]:
                 freeTime.append([busy[1], cal[i][0]])
                 busy = list(cal[i])
             else:
                 busy[1] = max( busy[1], cal[i][1])
-        freeTime.append([time[0],cal[0][0]])
         freeTime.append([cal[-1][1],time[1]]) 
-        freeTime.sort(key = lambda x:x[0])
-        return freeTime
+        result = filter(lambda  x:x[0] != x[1] , freeTime)
+        return list(result)
     free1 = get_free(cal1,time)
     free2 = get_free(cal2,time)
     print(free1,free2)
-calendar1 = [(20,100),(200,230),(250,330),(450,500)]
+calendar1 = [(0,100),(200,230),(250,330),(450,500)]
 calendar2 = [(30,100),(100,130),(250,330)]
 duration = 10
 time = (0,500)
