ced1b08e5710057a90f0d8129934489e0d0b46b1
unknown <g8749979677@gmail.com>
2021-06-13 22:39:23 -0700

    simplify the logic

--- easy/leetCode594.py
+++ easy/leetCode594.py
@@ -1,25 +1,16 @@
 # 594. Longest Harmonious Subsequence
 def findLHS(nums):
-    numMap,countMap, maxVal = {},{}, 0
+    numMap, maxVal = {},{}, 0
     for num in nums:
         if num in numMap:
             numMap[num] += 1
         else:
             numMap[num] = 1
-
-    if len(numMap) == 1:return 0
-
-    for key in numMap.keys():
-        if (key - 1) in numMap and (key + 1) in numMap:
-            countMap[key]  = numMap[key] + max(numMap[key -1], numMap[key + 1])
-        elif (key - 1) in numMap:
-            countMap[key]  = numMap[key] + numMap[key - 1]
-        elif (key + 1) in numMap:
-            countMap[key]  = numMap[key] + numMap[key + 1]
-       
-    for key in countMap.keys():
-        maxVal = max(maxVal, countMap[key])
-
+    
+    for i in numMap:
+        if i + 1 in numMap:
+            maxVal = max((numMap[i] + numMap[i + 1]), maxVal)
+    
     return maxVal
 
 print(findLHS([1,2,2,2,2,5,5,5,6,6,6]))
