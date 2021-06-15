1a0c913bea0ca421223845ec1ce97d82aef14f8c
zhenming yang <g874997967@gmail.com>
2021-06-15 09:57:51 -0700

    updates

--- easy/leetCode605.py
+++ easy/leetCode605.py
@@ -4,7 +4,6 @@ def canPlaceFlowers(flowerbed, n):
         s = len(flowerbed)
         bed = [0] + flowerbed + [0]
         for i in range(1, s+1):
-            
             if bed[i] == bed[i-1] == bed[i+1] == 0:
                 bed[i] = 1
                 n -= 1
